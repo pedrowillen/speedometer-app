0b7104b21f4351efbce62b1b8ea3aada87f8707c
Pedro Willen <pedrownsmcpe@gmail.com>
2024-05-09 15:54:45 -0400

    Adding speedometer

--- speedometer.js
+++ speedometer.js
@@ -2,24 +2,34 @@ const speedElement = document.querySelector('#speed');
 const startBtn = document.querySelector('#start');
 const stopBtn = document.querySelector('#stop');
 
-startBtn.addEventListener('click', () => {
-    function handleSuccess(position) {
+let watchID = null;
 
+startBtn.addEventListener('click', () => {
+    if (watchID) return
+
+    function handleSuccess(position) {
+        // console.log(position);
+        speedElement.innerText = position.coords.speed ?
+         (position.coords.speed * 3.6).toFixed(1) : 0
     }
 
     function handleError(error) {
-
+        console.log(error.msg)
     }
 
     const options = { enableHighAccuracy: true }
 
-    navigator.geolocation.watchPosition(handleSuccess, handleError, options);
+    watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, options);
 
     startBtn.classList.add('d-none');
     stopBtn.classList.remove('d-none');
 })
 
 stopBtn.addEventListener('click', () => {
+    if(!watchID) return
+
+    navigator.geolocation.clearWatch(watchID);
+    watchID = null;
     stopBtn.classList.add('d-none');
     startBtn.classList.remove('d-none');
 })
